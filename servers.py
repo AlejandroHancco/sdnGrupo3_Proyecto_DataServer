718c8cc283114f503a2ae53ce800e116dcff8400
AlejandroHancco <142966841+AlejandroHancco@users.noreply.github.com>
2025-07-19 00:02:12 -0500

    Update servers.py

--- servers.py
+++ servers.py
@@ -1,53 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template
 import multiprocessing
 
 # ---------- Módulo del servidor en puerto 9000 ----------
 def server_9000():
-    app = Flask("server_9000")
+    app = Flask("server_9000", template_folder='templates')
 
     @app.route('/')
     def home():
-        return "Servidor en puerto 9000"
+        return render_template('page_9000.html')
 
     app.run(host='0.0.0.0', port=9000)
 
 # ---------- Módulo del servidor en puerto 9001 ----------
 def server_9001():
-    app = Flask("server_9001")
+    app = Flask("server_9001", template_folder='templates')
 
     @app.route('/')
     def home():
-        return "Servidor en puerto 9001"
+        return render_template('circuitos.html')
 
     app.run(host='0.0.0.0', port=9001)
 
 # ---------- Módulo del servidor en puerto 9002 ----------
 def server_9002():
-    app = Flask("server_9002")
+    app = Flask("server_9002", template_folder='templates')
 
     @app.route('/')
     def home():
-        return "Servidor en puerto 9002"
+        return render_template('tecom2.html')
 
     app.run(host='0.0.0.0', port=9002)
 
 # ---------- Módulo del servidor en puerto 9003 ----------
 def server_9003():
-    app = Flask("server_9003")
+    app = Flask("server_9003", template_folder='templates')
 
     @app.route('/')
     def home():
-        return "Servidor en puerto 9003"
+        return render_template('ciber.html')
 
     app.run(host='0.0.0.0', port=9003)
 
 # ---------- Módulo del servidor en puerto 9004 ----------
 def server_9004():
-    app = Flask("server_9004")
+    app = Flask("server_9004", template_folder='templates')
 
     @app.route('/')
     def home():
-        return "Servidor en puerto 9004"
+        return render_template('circuitos.html')
 
     app.run(host='0.0.0.0', port=9004)
 
